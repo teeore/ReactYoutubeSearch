2005159cd00a190463d0efa927c4955019035df3
Tayo Oredola <toredola@its-macbook-pro.attlocal.net>
2017-01-01 00:50:43 -0600

    uploaded local files to git repository

---
+++ src/components/search_bar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        //record property 'term' on state
+        this.state = { term: '' };
+    }
+
+    //define render method to display in DOM
+    render() {
+        // return <input onChange={event => console.log(event.target.value)} />;
+        return (
+        	<div className="search-bar">
+            <input
+	            value={this.state.term}
+	            onChange={event => this.onInputChange(event.target.value)} />
+            </div>
+
+        );
+    }
+
+    onInputChange(term) {
+      this.setState({term});
+      this.props.onSearchTermChange(term);
+    }
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+//youtube api key from console.developers.google.com
+const API_KEY = 'AIzaSyDXJnVG_j7Cl4km0OCXcCd2epipsxf9Qf4';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+
+	}
+
+	videoSearch(term) {
+		//initial search when the app is run to prevent displaying empty divs
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos : videos,
+				selectedVideo : videos[0]
+			});
+		});
+	}
+
+	render() {
+
+		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300)
+		return (
+			<div>
+				<SearchBar onSearchTermChange = {videoSearch}/>
+				<VideoDetail video= { this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos = { this.state.videos } />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+//install npm package 'youtube api search';
